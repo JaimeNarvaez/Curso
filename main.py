0f4b807d270f9eb836add665d9e19b6f3a7ef837
JaimeNarvaez <jjno1999@gmail.com>
2024-09-12 17:33:55 -0500

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+)
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0,0.0, -5)
+
+    # Variables para el control de la rotación
+    x_rotation = 0
+    y_rotation = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        # Capturar las teclas presionadas
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            y_rotation -= 1
+        if keys[pygame.K_RIGHT]:
+            y_rotation += 1
+        if keys[pygame.K_UP]:
+            x_rotation -= 1
+        if keys[pygame.K_DOWN]:
+            x_rotation += 1
+
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+        # Aplicar las rotaciones
+        glPushMatrix()
+        glRotatef(x_rotation, 1, 0, 0)
+        glRotatef(y_rotation, 0, 1, 0)
+        Cube()
+        glPopMatrix()
+
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+if __name__ == "__main__":
+    main()
